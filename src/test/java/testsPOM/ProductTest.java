83d64dc8d20c205bf0b16a364c54f71805ce2305
Ibraim Asanov <iasanov@nemetschek.bg>
2024-03-19 20:17:11 +0200

    Soft Asserts example

--- src/test/java/testsPOM/ProductTest.java
+++ src/test/java/testsPOM/ProductTest.java
@@ -1,4 +1,35 @@
 package testsPOM;
 
-public class ProductTest {
+import baseTC.TestUtil;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pagesPOM.LoginPage;
+import pagesPOM.ProductsPage;
+
+public class ProductTest extends TestUtil {
+    @Test
+    public void addItemToTheCart(){
+        LoginPage loginPage = new LoginPage(driver);
+        ProductsPage productsPage = loginPage.login("standard_user", "secret_sauce");
+
+        productsPage.addItemToTheCart("bike-light");
+        Assert.assertEquals(productsPage.getItemsInTheCart(), 1);
+    }
+
+    @Test
+    public void addItemsToTheCart(){
+        LoginPage loginPage = new LoginPage(driver);
+        ProductsPage productsPage = loginPage.login("standard_user", "secret_sauce");
+
+        SoftAssert softAssert = new SoftAssert();
+
+        productsPage.addItemToTheCart("bike-light");
+        softAssert.assertEquals(productsPage.getItemsInTheCart(), 1, "Because we added only one item so far");
+
+        productsPage.addItemToTheCart("backpack");
+        softAssert.assertEquals(productsPage.getItemsInTheCart(), 2, "Because we added 2nd item");
+
+        softAssert.assertAll();
+    }
 }
