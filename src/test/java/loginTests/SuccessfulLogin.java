a206f4471dc50b3058d10232ed247f1b877966a7
Ibraim Asanov <iasanov@nemetschek.bg>
2024-03-21 19:17:18 +0200

    fix failing test

--- src/test/java/loginTests/SuccessfulLogin.java
+++ src/test/java/loginTests/SuccessfulLogin.java
@@ -27,7 +27,7 @@ public class SuccessfulLogin extends TestUtil {
         WebElement loginBtn = driver.findElement(By.cssSelector("[name=login-button]"));
         loginBtn.click();
 
-        WebElement productsPageTitle = driver.findElement(By.xpath("//span[contains(text(),'Productss')]"));
+        WebElement productsPageTitle = driver.findElement(By.xpath("//span[contains(text(),'Products')]"));
         Assert.assertTrue(productsPageTitle.isDisplayed());
     }
 }
